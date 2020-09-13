17733ee60ec58bf0559c2a2d138b051e0d57186d
harsh_kasyap <harshkasyapAgmail.com>
2020-09-13 19:17:11 +0530

    Submitting Factorial Program

--- Assignment_1/factorial.py
+++ Assignment_1/factorial.py
@@ -1,7 +1,17 @@
 # Python program to find the factorial of a number provided by the user.
 
 def factorial(num):
-#Logic for the factorial program
+    #Logic for the factorial program
+    
+    factorial = 1
+
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num + 1):
+            factorial = factorial*i
     print("The factorial of",num,"is",factorial)
 
 # To take input from the user
